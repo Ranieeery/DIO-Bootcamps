135025d50690c91b85a21b5c7135b6e0ea3ebf33
Ranieeery <raniery2003@hotmail.com>
2024-08-08 20:50:34 -0300

    feat: challenge part 1 and 2

---
+++ Claro - Java com Spring Boot/Desafios/Verificando Velocidade Média e Queda de Conexão/Parte 2/Main.java
@@ -0,0 +1,36 @@
+import java.util.Objects;
+import java.util.Scanner;
+
+public class Main {
+
+    // TODO: Preencha a função para verificar queda de conexão
+    public static String verificarQuedaConexao(String[] velocidades) {
+        String controle = "Sem Quedas";
+
+        for (String velocidade : velocidades){
+            if (Objects.equals(velocidade, "0")) {
+                controle = "Queda de Conexao";
+                break;
+            }
+        }
+        return controle;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Solicitando ao usuário a lista de velocidades de conexão registradas a cada hora
+        String input = scanner.nextLine();
+
+        // Convertendo a entrada em uma lista de strings
+        String[] velocidades = input.split(",");
+
+        // Verificando se houve queda de conexão
+        String resultado = verificarQuedaConexao(velocidades);
+
+        // Exibindo o resultado da verificação
+        System.out.println(resultado);
+
+        scanner.close();
+    }
+}
