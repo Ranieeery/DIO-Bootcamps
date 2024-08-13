d5e1bc7db9fc01cf21ef5f30580ecdde3bdad263
Ranieeery <raniery2003@hotmail.com>
2024-08-12 23:42:06 -0300

    chore: Update Maven dependencies and add Mockito for unit testing

---
+++ Claro - Java com Spring Boot/Mockito/src/test/java/org/mockito/example/ListaTeste.java
@@ -0,0 +1,8 @@
+package org.mockito.example;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoEx)
+public class ListaTeste {
+}
