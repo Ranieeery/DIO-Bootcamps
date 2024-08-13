bdb4a863634a1930cabea2042b878b91766afe48
Ranieeery <raniery2003@hotmail.com>
2024-08-12 23:51:02 -0300

    refactor: Add Mockito for unit testing and update Maven dependencies

--- Claro - Java com Spring Boot/Mockito/src/test/java/org/mockito/example/ListaTeste.java
+++ Claro - Java com Spring Boot/Mockito/src/test/java/org/mockito/example/ListaTeste.java
@@ -1,8 +1,24 @@
 package org.mockito.example;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-@ExtendWith(MockitoEx)
+import java.util.List;
+
+@ExtendWith(MockitoExtension.class)
 public class ListaTeste {
+
+    @Mock
+    private List<String> letras;
+
+    @Test
+    void adicionarItemLista() {
+        Mockito.when(letras.getFirst()).thenReturn("B");
+
+        Assertions.assertEquals("B", letras.getFirst());
+    }
 }
