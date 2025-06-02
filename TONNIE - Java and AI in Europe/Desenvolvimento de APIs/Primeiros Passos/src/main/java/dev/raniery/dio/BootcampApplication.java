5f829ff3b9e95ddc3b919d77edd38c5ea1622308
Ranieeery <raniery2003@hotmail.com>
2025-06-02 16:02:36 -0300

    feat: add ConversorJson and ViaCepResponse classes, update BootcampApplication with JSON conversion logic

--- TONNIE - Java and AI in Europe/Desenvolvimento de APIs/Primeiros Passos/src/main/java/dev/raniery/dio/BootcampApplication.java
+++ TONNIE - Java and AI in Europe/Desenvolvimento de APIs/Primeiros Passos/src/main/java/dev/raniery/dio/BootcampApplication.java
@@ -1,13 +1,31 @@
 package dev.raniery.dio;
 
+import com.google.gson.Gson;
+import dev.raniery.dio.app.ConversorJson;
+import dev.raniery.dio.app.ViaCepResponse;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BootcampApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BootcampApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BootcampApplication.class, args);
+    }
 
+    @Bean
+    public CommandLineRunner run(ConversorJson conversor) throws Exception {
+        return args -> {
+            String json = "{\"cep\": \"00001-001\",\"logradouro\": \"Praça XV\", \"localidade\": \"Rio de Janeiro\"}";
+            ViaCepResponse response = conversor.converter(json);
+            System.out.println("Dados CEP: " + response);
+        };
+    }
+
+    @Bean
+    public Gson gson() {
+        return new Gson();
+    }
 }
---
+++ TONNIE - Java and AI in Europe/Desenvolvimento de APIs/Primeiros Passos/src/main/java/dev/raniery/dio/app/ConversorJson.java
@@ -0,0 +1,18 @@
+package dev.raniery.dio.app;
+
+import com.google.gson.Gson;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ConversorJson {
+
+    private final Gson gson;
+
+    public ConversorJson(Gson gson) {
+        this.gson = gson;
+    }
+
+    public ViaCepResponse converter(String json) {
+        return gson.fromJson(json, ViaCepResponse.class);
+    }
+}
