7648b772942888575d0d4c61076610e4cd8da275
Ranieeery <raniery2003@hotmail.com>
2025-06-03 11:10:29 -0300

    feat: implement REST API for Cliente and Endereco with service and repository layers with GoF

---
+++ TONNIE - Java and AI in Europe/Desenvolvimento de APIs/Design Patterns com Java - Dos Clássicos (GoF) ao Spring Framework/src/main/java/dev/raniery/dio/service/ClienteService.java
@@ -0,0 +1,17 @@
+package dev.raniery.dio.service;
+
+import dev.raniery.dio.model.Cliente;
+
+public interface ClienteService {
+
+    Iterable<Cliente> buscarTodos();
+
+    Cliente buscarPorId(Long id);
+
+    void inserir(Cliente cliente);
+
+    void atualizar(Long id, Cliente cliente);
+
+    void deletar(Long id);
+
+}
