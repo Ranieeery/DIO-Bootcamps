be7b3452c43937fee940d51006d0f98f466063c1
Ranieeery <raniery2003@hotmail.com>
2025-06-03 20:00:44 -0300

    feat: remove order column from cards and rename block/unblock columns in blocks

---
+++ TONNIE - Java and AI in Europe/Desenvolvimento de APIs/Integração Java com Banco de Dados/TaskBoard/src/main/resources/db/changelog/migrations/V5__remove_cards_order_rename_blocks_columns.sql
@@ -0,0 +1,19 @@
+--liquibase formatted sql
+--changeset raniery:v5
+--comment: remove order column from cards and rename block/unblock columns in blocks
+
+ALTER TABLE CARDS DROP COLUMN `order`;
+
+ALTER TABLE BLOCKS CHANGE COLUMN block_at blocked_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP;
+
+ALTER TABLE BLOCKS CHANGE COLUMN block_reason blocked_reason VARCHAR(255) NOT NULL;
+
+ALTER TABLE BLOCKS CHANGE COLUMN unblock_at unblocked_at TIMESTAMP NULL;
+
+ALTER TABLE BLOCKS CHANGE COLUMN unblock_reason unblocked_reason VARCHAR(255) NOT NULL;
+
+--rollback ALTER TABLE CARDS ADD COLUMN `order` INT NOT NULL DEFAULT 0;
+--rollback ALTER TABLE BLOCKS CHANGE COLUMN blocked_at block_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP;
+--rollback ALTER TABLE BLOCKS CHANGE COLUMN blocked_reason block_reason VARCHAR(255) NOT NULL;
+--rollback ALTER TABLE BLOCKS CHANGE COLUMN unblocked_at unblock_at TIMESTAMP NULL;
+--rollback ALTER TABLE BLOCKS CHANGE COLUMN unblocked_reason unblock_reason VARCHAR(255) NOT NULL;
