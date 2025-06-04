9e109ae041d93420f5a604f0833bdce09ab53a6e
Ranieeery <raniery2003@hotmail.com>
2025-06-03 22:33:22 -0300

    feat: alter blocks table to allow null values for unblocked_reason column

--- TONNIE - Java and AI in Europe/Desenvolvimento de APIs/Integração Java com Banco de Dados/TaskBoard/src/main/resources/db/changelog/migrations/V6__alter_table_blocks_column_unblocked_reason.sql
+++ TONNIE - Java and AI in Europe/Desenvolvimento de APIs/Integração Java com Banco de Dados/TaskBoard/src/main/resources/db/changelog/migrations/V6__alter_table_blocks_column_unblocked_reason.sql
@@ -1,15 +1,7 @@
 --liquibase formatted sql
---changeset raniery:v4
---comment: create blocks table
+--changeset raniery:v6
+--comment: alter table blocks column unblocked reason
 
-CREATE TABLE BLOCKS (
-    id             BIGINT AUTO_INCREMENT PRIMARY KEY,
-    block_at       TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    block_reason   VARCHAR(255) NOT NULL,
-    unblock_at     TIMESTAMP NULL,
-    unblock_reason VARCHAR(255) NOT NULL,
-    card_id        BIGINT       NOT NULL,
-    CONSTRAINT cards__blocks_fk FOREIGN KEY (card_id) REFERENCES CARDS (id) ON DELETE CASCADE
-) ENGINE=InnoDB;
+ALTER TABLE BLOCKS MODIFY COLUMN unblocked_reason VARCHAR(255) NULL;
 
---rollback DROP TABLE BLOCKS
+--rollback ALTER TABLE BLOCKS MODIFY COLUMN unblocked_reason VARCHAR(255) NOT NULL;
